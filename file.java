831facd54a059c5b0eefaa4950bd56c63aae9bf4
Aditya Chaganti <chaganti.aditya9@gmail.com>
2019-06-05 14:10:53 +0530

    New files and modified files added to repo

--- file.java
+++ file.java
@@ -2,7 +2,8 @@ class demo {
 	public static void main(String[] arguments)
 	{
 		system.out.println("Hello git");
-system.out.println("Git is very easy to understand", +git):
+system.out.println("Git is very easy to understand", +git);
+system.out.print;m("new changes");
 	}
 }
 
