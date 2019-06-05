d603e9701953b254b363c8204684f64c0f5b841d
Aditya Chaganti <chaganti.aditya9@gmail.com>
2019-06-05 17:00:12 +0530

    .gitignore

--- file.java
+++
@@ -1,9 +0,0 @@
-class demo {
-	public static void main(String[] arguments)
-	{
-		system.out.println("Hello git");
-system.out.println("Git is very easy to understand", +git);
-system.out.print;m("new changes to git");
-	}
-}
-
